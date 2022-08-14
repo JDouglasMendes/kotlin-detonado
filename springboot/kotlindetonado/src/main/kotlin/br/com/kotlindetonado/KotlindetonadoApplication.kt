41eb95df2c7ad37c9cee922c4498c5d46d4b26e3
Douglas Mendes <douglasaleixomendes@gmail.com>
2022-08-13 21:14:54 -0300

    init project

---
+++ springboot/kotlindetonado/src/main/kotlin/br/com/kotlindetonado/KotlindetonadoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.kotlindetonado
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlindetonadoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlindetonadoApplication>(*args)
+}
---
+++ springboot/kotlindetonado/src/test/kotlin/br/com/kotlindetonado/KotlindetonadoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.kotlindetonado
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlindetonadoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
